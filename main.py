c0dc43c2a5223dd564ce906914c2b9d1ff10ad4e
Ivan Obuhov <65172086+Obuhov@users.noreply.github.com>
2022-10-31 19:33:00 +0500

    1)update window. fullscreen + HardwareAcRen 2)Surface 3)Color

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import pygame
 pygame.init()
 
-# Окно
-pygame.display.set_mode((800, 600))
+# Окно. HWSURFACE- (HardwareAcRen) аппаратное ускорение отрисовки(Fullscr)
+sc = pygame.display.set_mode((800, 600), pygame.HWSURFACE | pygame.FULLSCREEN)
 pygame.display.set_caption('Major League')
 pygame.display.set_icon(pygame.image.load("img/logo/logoTest.ico"))
+
+white = (255, 255, 255)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+red = (255, 0, 0)
+
+#(R, G, B) 0 - отсутствие цв. 255 - полная насыщенность цв.
+# Буферизация. Перерисовка опр. Объекта (При указании)
+pygame.display.update()
 
 # ФПС
 clock = pygame.time.Clock()
